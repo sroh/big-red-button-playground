9ec60cb79ba77694313fffc9e885cd25f1c6fc8e
srohde <sven.rohde@fdmediagroep.nl>
2019-10-22 09:29:55 +0200

    initial commit, playground test script added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # big-red-button-playground
+
 NodeJS playground project to control the [Dream Cheeky: BIG RED BUTTON]
+
+## Based on the NPM package : https://www.npmjs.com/package/big-red-button
---
+++ big-red-button-event-listener-script.js
@@ -0,0 +1,29 @@
+var BigRedButton = require("big-red-button");
+
+var bigRedButtons = [];
+
+for (var i = 0; i < BigRedButton.deviceCount(); i++) {
+  console.log("opening BigRedButton", i);
+
+  bigRedButtons.push(new BigRedButton.BigRedButton(i));
+
+  bigRedButtons[i].on("buttonPressed", function() {
+    console.log("Button pressed");
+  });
+
+  bigRedButtons[i].on("buttonReleased", function() {
+    console.log("Button released");
+  });
+
+  bigRedButtons[i].on("lidRaised", function() {
+    console.log("Lid raised");
+  });
+
+  bigRedButtons[i].on("lidClosed", function() {
+    console.log("Lid closed");
+  });
+
+  bigRedButtons[i].on("error", function(error) {
+    console.error(error);
+  });
+}
